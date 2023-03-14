51bf8fe065ab10a4bddb8d0a42bc4a8c220429fc
kirbi <Biehan@mail.ru>
2023-03-14 22:06:32 +0300

    date format

---
+++ informing_app/templatetags/date_filter.py
@@ -0,0 +1,8 @@
+from django import template
+from datetime import datetime
+
+register = template.Library()
+
+@register.filter(name='format')
+def date_format(date: datetime):
+    return date.strftime("%H:%M %d/%m/%Y")
--- users/views.py
+++ users/views.py
@@ -8,15 +8,12 @@ from .decorators import check_profile_activation
 @check_profile_activation
 def profile(request):
     if hasattr(request.user, 'student_profile'):
-        profile = Student_Profile.objects.filter(user=request.user).first()
-        context = { 'profile' : profile , 
+        context = { 'profile' : Student_Profile.objects.filter(user=request.user).first() , 
                    'courses': Course.objects.filter(groups = Student_Profile.objects.filter(user=request.user).first().group) }
     elif hasattr(request.user, 'teacher_profile'):
-        profile = Teacher_Profile.objects.filter(user=request.user).first()
-        context = { 'profile': profile ,
+        context = { 'profile': Teacher_Profile.objects.filter(user=request.user).first() ,
                    'courses': Course.objects.filter(author = Teacher_Profile.objects.filter(user=request.user).first())}
     elif hasattr(request.user, 'admin_profile'):
-        profile = Admin_Profile.objects.filter(user=request.user).first()
-        context = { 'profile': profile }
+        context = { 'profile': Admin_Profile.objects.filter(user=request.user).first() }
     return render(request, 'users/profile.html', context)
 
